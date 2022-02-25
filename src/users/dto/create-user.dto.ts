358222bee095bdb49cccf3647e477cb21efea7c5
jack <relee6203@gmail.com>
2022-02-25 15:45:38 +0900

    make custom validation decorator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,16 +1,33 @@
-import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+import { BadRequestException } from '@nestjs/common';
+import { Transform } from 'class-transformer';
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { NotIn } from 'src/decorator/NotIn';
 
 export class CreateUserDto {
+  @Transform(({ value }) => value.trim())
+  @NotIn('password', {
+    message: 'password는 name과 같은 문자열을 포함할 수 없습니다.',
+  })
   @IsString()
-  @MinLength(1)
-  @MaxLength(20)
-  name: string;
-
+  @MinLength(2)
+  @MaxLength(30)
+  readonly name: string;
+  @IsString()
   @IsEmail()
-  email: string;
+  @MaxLength(60)
+  readonly email: string;
 
   @IsString()
-  @MinLength(8)
-  @MaxLength(16)
-  password: string;
+  @Matches(/^[A-Za-z\d!@#$%^&*()]{8,30}$/)
+  readonly password: string;
+
+  asd() {
+    return `${this.email} ${this.password}`;
+  }
 }
